6c1ee3520db65e0f2cfe55bb15cfc6e9def1c9ab
egor.korobitsin <egor.korobitsin@itechart-group.com>
2020-04-03 18:12:40 +0300

    initial

---
+++ src/main/java/com/itechartgroup/telemedpoc/TelemedPoCApplication.java
@@ -0,0 +1,13 @@
+package com.itechartgroup.telemedpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelemedPoCApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelemedPoCApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itechartgroup/telemedpoc/TelemedPoCApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itechartgroup.telemedpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelemedPoCApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
