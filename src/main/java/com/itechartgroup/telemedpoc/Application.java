9e0f776d95c08d48fb3091f92bac8cdb726da57a
egor.korobitsin <egor.korobitsin@itechart-group.com>
2020-04-04 17:45:20 +0300

    initial

--- src/main/java/com/itechartgroup/telemedpoc/Application.java
+++ src/main/java/com/itechartgroup/telemedpoc/Application.java
@@ -1,15 +1,13 @@
 package com.itechartgroup.telemedpoc;
 
-import com.itechartgroup.telemedpoc.config.TwilioConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
 }
