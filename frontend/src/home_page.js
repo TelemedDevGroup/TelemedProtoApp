0f72b0d4e87b53f7cf3e21ab9c714fffb8cddab2
omihalap <c-omihalap@neogov.net>
2020-04-08 11:10:50 +0300

    [add]-component-for-link

--- frontend/src/home_page.js
+++ frontend/src/home_page.js
@@ -14,87 +14,96 @@ import {
 } from "@material-ui/core";
 import AccountBoxIcon from "@material-ui/icons/AccountBox";
 import LocalHospitalIcon from "@material-ui/icons/LocalHospital";
-import { Link } from "react-router-dom";
+import { Link as RouterLink } from "react-router-dom";
 import AccountAPI from "./test_data/test_users.js";
 
+function ListItemLink(props) {
+  const { icon, primary, to } = props;
 
-const HomePage = () => { 
-    return (
-      <Container component="main" maxWidth="sm">
-        <Grid container>
-          <Typography variant="h4" color="primary" align="center">
-            Welcome to Telemedicine Demo &nbsp;&nbsp;
-          </Typography>
-          <Avatar src="./telemed-logo.png" />
-        </Grid>
-        <Grid container justify="center" spacing={8}>
-          <Grid item xs={6}>
-          <Typography variant="h6">
-           Sign In
-          </Typography>
-            <form>
-              <TextField
-                variant="outlined"
-                margin="normal"
-                required
-                fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-              />
-              <TextField
-                variant="outlined"
-                margin="normal"
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-              />
+  const renderLink = React.useMemo(
+    () =>
+      React.forwardRef((itemProps, ref) => (
+        <RouterLink to={to} ref={ref} {...itemProps} />
+      )),
+    [to]
+  );
 
-              <Button
-                type="submit"
-                fullWidth
-                size="large"
-                variant="contained"
-                color="primary"
-              >
-                Login
-              </Button>
-            </form>
-          </Grid>
-          <Grid item xs={6}>
-            <Typography variant="h6">
-              Or use demo account
-            </Typography>
-            <List>
-              {AccountAPI.all().map((user) => (
-                <ListItem key={user.id} >
-                  <ListItemIcon>
-                    {user.user_type === "Doctor" ? (
-                      <LocalHospitalIcon />
-                    ) : (
-                      <AccountBoxIcon />
-                    )}
-                  </ListItemIcon>
-                  <ListItemText >
-                    <Link to={calcPath2Board(user)}>
-                      {user.user_type}: {user.name}
-                    </Link>
-                  </ListItemText>
-                </ListItem>
-              ))}
-            </List>
-          </Grid>
-        </Grid>
-
-        <Typography variant="body1" color="primary"  align="center">
-          v{process.env.REACT_APP_DEMO_VERSION}
-        </Typography>
-      </Container>
-    ); 
+  return (
+    <li>
+      <ListItem button component={renderLink}>
+        <ListItemIcon>
+          {props.icon === "Doctor" ? <LocalHospitalIcon /> : <AccountBoxIcon />}
+        </ListItemIcon>
+        <ListItemText primary={primary} />
+      </ListItem>
+    </li>
+  );
 }
+
+const HomePage = () => {
+  return (
+    <Container component="main" maxWidth="sm">
+      <Grid container>
+        <Typography variant="h4" color="primary" align="center">
+          Welcome to Telemedicine Demo &nbsp;&nbsp;
+        </Typography>
+        <Avatar src="./telemed-logo.png" />
+      </Grid>
+      <Grid container justify="center" spacing={8}>
+        <Grid item xs={6}>
+          <Typography variant="h6">Sign In</Typography>
+          <form>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+            />
+
+            <Button
+              type="submit"
+              fullWidth
+              size="large"
+              variant="contained"
+              color="primary"
+            >
+              Login
+            </Button>
+          </form>
+        </Grid>
+        <Grid item xs={6}>
+          <Typography variant="h6">Or use demo account</Typography>
+          <List>
+            {AccountAPI.all().map((user) => (
+              <ListItemLink
+                to={calcPath2Board(user)}
+                primary={user.user_type + " " + user.name}
+                icon={user.user_type}
+              />
+            ))}
+          </List>
+        </Grid>
+      </Grid>
+
+      <Typography variant="body1" color="primary" align="center">
+        v{process.env.REACT_APP_DEMO_VERSION}
+      </Typography>
+    </Container>
+  );
+};
 
 function calcPath2Board(user) {
   let path = user.user_type === "Doctor" ? "/doctor/" : "/user/";
