e409caa4746149d522657b67ad8d08d99490cb05
omihalap <c-omihalap@neogov.net>
2020-04-08 11:02:44 +0300

    semantic-ui-changed-to-material

--- frontend/src/home_page.js
+++ frontend/src/home_page.js
@@ -1,69 +1,102 @@
-import React, {Component} from 'react';
-import './home_page.css';
-import { Button, Form, Grid, Header, Image, Segment, Divider, List } from 'semantic-ui-react'
-import { Link } from 'react-router-dom'
-import AccountAPI from './test_data/test_users.js'
+import React from "react";
+import "./home_page.css";
+import {
+  Button,
+  TextField,
+  Container,
+  Grid,
+  Typography,
+  Avatar,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+} from "@material-ui/core";
+import AccountBoxIcon from "@material-ui/icons/AccountBox";
+import LocalHospitalIcon from "@material-ui/icons/LocalHospital";
+import { Link } from "react-router-dom";
+import AccountAPI from "./test_data/test_users.js";
 
-class HomePage extends Component {
-  constructor() {
-    super();
-    this.state = {
-      default_login: "Aibolit",
-    };
-  }
-  render() {
+
+const HomePage = () => { 
     return (
-      <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
-          <Grid.Column style={{ maxWidth: 650 }}>
-            <Header as='h2' color='teal' textAlign='center'>Welcome to Telemedicine Demo &nbsp;&nbsp;
-              <Image src='./telemed-logo.png' />
-            </Header>
-            <Segment placeholder>
-              <Grid columns={2} relaxed='very' stackable>
-                <Grid.Column>
-                  <Form>
-                    <Form.Input fluid icon='user' iconPosition='left' placeholder='E-mail address' />
-                    <Form.Input
-                      fluid
-                      icon='lock'
-                      iconPosition='left'
-                      placeholder='Password'
-                      type='password'
-                    />
+      <Container component="main" maxWidth="sm">
+        <Grid container>
+          <Typography variant="h4" color="primary" align="center">
+            Welcome to Telemedicine Demo &nbsp;&nbsp;
+          </Typography>
+          <Avatar src="./telemed-logo.png" />
+        </Grid>
+        <Grid container justify="center" spacing={8}>
+          <Grid item xs={6}>
+          <Typography variant="h6">
+           Sign In
+          </Typography>
+            <form>
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+              />
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+              />
 
-                    <Button color='teal' fluid size='large'>
-                      Login
-                  </Button>
-                  </Form>
-                </Grid.Column>
-
-                <Grid.Column verticalAlign='middle' textAlign='center'>
-                  <Header as='h4' color='teal' textAlign='center'>Use Demo account</Header>
-                  <List floated='left'>
-                  { AccountAPI.all().map( (user, index) => (
-                        <List.Item
-                          icon={user.user_type === "Doctor" ? "doctor" : "user"}
-                          key={user.id}
-                          content={ <Link to={calcPasth2Board(user)}> {user.user_type}: {user.name}  </Link> }
-                        />
-                    ))
-                  }
-                  </List>
-                </Grid.Column>
-              </Grid>
-
-              <Divider vertical> Or </Divider>
-            </Segment>
-            <Header as='h4' color='teal' textAlign='center'>v{process.env.REACT_APP_DEMO_VERSION}  </Header>
-          </Grid.Column>
+              <Button
+                type="submit"
+                fullWidth
+                size="large"
+                variant="contained"
+                color="primary"
+              >
+                Login
+              </Button>
+            </form>
+          </Grid>
+          <Grid item xs={6}>
+            <Typography variant="h6">
+              Or use demo account
+            </Typography>
+            <List>
+              {AccountAPI.all().map((user) => (
+                <ListItem key={user.id} >
+                  <ListItemIcon>
+                    {user.user_type === "Doctor" ? (
+                      <LocalHospitalIcon />
+                    ) : (
+                      <AccountBoxIcon />
+                    )}
+                  </ListItemIcon>
+                  <ListItemText >
+                    <Link to={calcPath2Board(user)}>
+                      {user.user_type}: {user.name}
+                    </Link>
+                  </ListItemText>
+                </ListItem>
+              ))}
+            </List>
+          </Grid>
         </Grid>
 
-
-    );
-  }
+        <Typography variant="body1" color="primary"  align="center">
+          v{process.env.REACT_APP_DEMO_VERSION}
+        </Typography>
+      </Container>
+    ); 
 }
 
-function calcPasth2Board(user) {
+function calcPath2Board(user) {
   let path = user.user_type === "Doctor" ? "/doctor/" : "/user/";
   path += user.id + "/board";
   console.log("test user:", user, ";  PATH= ", path);
