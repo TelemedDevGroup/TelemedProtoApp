3c82ec82c1bc76c1cd23d40f4adc13b3edc9528b
omihalap <c-omihalap@neogov.net>
2020-04-09 19:13:06 +0300

    [refactor]-chat-completely-migrated-to-material-ui

--- frontend/src/components/Chat/ChatContainer.js
+++ frontend/src/components/Chat/ChatContainer.js
@@ -1,28 +1,67 @@
 import React, { useState } from "react";
-import { Message, Input, Image } from "semantic-ui-react";
-import {
-  Button,
-  Grid,
-  Typography,
-  List,
-  Card,
-  ListItem,
-  ListItemText,
-} from "@material-ui/core";
+import { Button, Grid, Typography, TextField } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import VideoCallIcon from "@material-ui/icons/VideoCall";
 
+const useStyles = makeStyles({
+  container: {
+    margin: "1rem",
+  },
+  message: {
+    border: "1px solid gray",
+    background: "WhiteSmoke",
+    padding: "0.8rem",
+    marginTop: "1rem",
+    borderRadius: "8px",
+    width: "48%",
+  },
+  senderName: {
+    fontSize: "1.2rem",
+  },
+  partner: {
+    border: "1px solid #00B5AD",
+    background: "#F2FBFB",
+    alignSelf: "flex-end",
+  },
+
+  image: {
+    padding: "1rem 0",
+    width: "100%",
+  },
+
+  videoButton: {
+    background: "#00B5AD",
+    color: "white",
+    height: "40px",
+    "&:hover": {
+      background: "#00B5AD",
+    },
+  },
+
+  sendButton: {
+    background: "#00B5AD",
+    color: "white",
+    minWidth: "7rem",
+    height: "3.85rem",
+    borderRadius: "0 4px 4px 0",
+    "&:hover": {
+      background: "#00B5AD",
+    },
+  },
+});
+
 const ChatContainer = ({ chatsData, partner, onClick }) => {
   let [inputData, setInputData] = useState("");
+  const classes = useStyles();
   return (
     <>
       {!chatsData ? (
         <p>Select conversation</p>
       ) : (
         <Grid
+          className={classes.container}
           container
           style={{
-            position: "relative",
             height: "700px",
           }}
         >
@@ -30,55 +69,65 @@ const ChatContainer = ({ chatsData, partner, onClick }) => {
             <Typography variant="h5">{partner}</Typography>
             <Button
               variant="contained"
-              color="primary"
+              className={classes.videoButton}
               startIcon={<VideoCallIcon>send</VideoCallIcon>}
             >
               Video Call
             </Button>
           </Grid>
-          <Grid item xs={12}>
-            <List
-              style={{
-                height: "80%",
-                overflowY: "auto",
-              }}
-            >
-              {chatsData.map((message, index) => (
-                <Message info={message.partner} size="small" key={index}>
-                  <Message.Header>{message.sender}</Message.Header>
-                  {message.attachment && (
-                    <Image
-                      size="medium"
-                      style={{ padding: "1.5em" }}
-                      src={message.attachment}
-                    ></Image>
-                  )}
-                  {message.message && <p>{message.message}</p>}
-                </Message>
-              ))}
-            </List>
+          <Grid
+            container
+            direction="column"
+            wrap="nowrap"
+            style={{
+              height: "80%",
+              overflowY: "auto",
+            }}
+          >
+            {chatsData.map((message, index) => (
+              <Grid
+                key={index}
+                className={`${classes.message} ${
+                  message.partner || classes.partner
+                }`}
+              >
+                <Typography className={classes.senderName} variant="h6">
+                  {message.sender}
+                </Typography>
+                {message.attachment && (
+                  <img
+                    className={classes.image}
+                    src={message.attachment}
+                    alt="attachment"
+                  ></img>
+                )}
+
+                {message.message && (
+                  <Typography variant="body1">{message.message}</Typography>
+                )}
+              </Grid>
+            ))}
           </Grid>
 
-          <Input
-            value={inputData}
-            onChange={(event) => setInputData(event.target.value)}
-            style={{
-              position: "absolute",
-              bottom: "0",
-              width: "100%",
-            }}
-            fluid
-            size="large"
-            action={{
-              color: "teal",
-              content: "Send",
-              onClick: () => {
+          <Grid container direction="row" alignContent="center" wrap="nowrap">
+            <TextField
+              fullWidth
+              id="message_input"
+              variant="outlined"
+              placeholder="Input your message..."
+              value={inputData}
+              onChange={(event) => setInputData(event.target.value)}
+            ></TextField>
+            <Button
+              className={classes.sendButton}
+              onClick={() => {
                 inputData && onClick(inputData);
                 setInputData("");
-              },
-            }}
-            placeholder="Input your message..."
-          />
+              }}
+            >
+              Send
+            </Button>
+          </Grid>
         </Grid>
       )}
     </>
