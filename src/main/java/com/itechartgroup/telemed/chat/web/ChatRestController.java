a69687876a08da937ce4dfb0dc5f53095b484411
egor.korobitsin <egor.korobitsin@itechart-group.com>
2020-04-10 15:13:14 +0300

    create server-side for Twillio trial video concept #18

--- src/main/java/com/itechartgroup/telemed/chat/web/ChatRestController.java
+++ src/main/java/com/itechartgroup/telemed/chat/web/ChatRestController.java
@@ -35,7 +35,7 @@ import static com.itechartgroup.telemed.chat.constant.ChatConstants.SESSION_ATTR
  */
 @RestController
 @AllArgsConstructor
-@RequestMapping("/api/chat")
+@RequestMapping("/chat")
 public class ChatRestController {
 
     private final ChatRoomService chatRoomService;
